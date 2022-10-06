b8ab44a4a9820b00266d681da705696d13abb4c3
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-10-06 18:35:06 +0300

    Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,41 @@
+﻿double[] CreateArrayRndInt(int size,double min,double max)
+{
+    double [] array = new double[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.NextDouble()*(max - min) + min;
+        array[i] = Math.Round(array[i],1,MidpointRounding.ToZero);
+    }
+    return array;
+}
+
+double [] arrayGlobal = CreateArrayRndInt(4,1,10);
+
+
+double EvenNumbers (double[] array)
+{
+    double maxNumber = array[0];
+    double minNumber = array[0];
+    double Difference  = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+      if (maxNumber<array[i])
+        {
+        maxNumber= array[i];
+        }
+        else if (minNumber > array[i])
+         {
+         minNumber= array[i];
+         }
+    }
+    Difference = maxNumber - minNumber;
+    return Difference;
+}
+double elementDifferens = EvenNumbers(arrayGlobal);
+elementDifferens = Math.Round(elementDifferens,1,MidpointRounding.ToZero);
+Console.WriteLine($"Разница между масимальным и минимальным элементом массива = {elementDifferens}");
+EvenNumbers(arrayGlobal);
+
+
